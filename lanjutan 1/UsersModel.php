3a5b622e4e7842a0b19f0c70bbd1d884df1ec64c
AhmadMauludin <ahmadmaldin1799@gmail.com>
2025-11-10 11:08:31 +0700

    Update UsersModel.php

--- lanjutan 1/UsersModel.php
+++ lanjutan 1/UsersModel.php
@@ -10,15 +10,9 @@ class UsersModel extends Model
     protected $primaryKey = 'id_user';
     protected $allowedFields = ['nama', 'username', 'password', 'role', 'foto'];
 
-    public function getUsersByNama($nama)
-    {
-        return $this->where('nama', $nama)->first();
-    }
-
     public function getUsersByUsername($username)
     {
         return $this->where('username', $username)->first();
     }
-
 }
 
