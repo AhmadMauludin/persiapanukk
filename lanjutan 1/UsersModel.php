76a6ef1680395d142995c2dd92f882aacdc2f34a
AhmadMauludin <ahmadmaldin1799@gmail.com>
2025-11-10 11:00:16 +0700

    Add UsersModel with user retrieval methods

--- lanjutan 1/UsersModel.php
+++ lanjutan 1/UsersModel.php
@@ -1,1 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id_user';
+    protected $allowedFields = ['nama', 'username', 'password', 'role', 'foto'];
+
+    public function getUsersByNama($nama)
+    {
+        return $this->where('nama', $nama)->first();
+    }
+
+    public function getUsersByUsername($username)
+    {
+        return $this->where('username', $username)->first();
+    }
+
+    public function search($keyword, $perPage)
+    {
+        return $this->like('nama', $keyword)
+            ->orLike('role', $keyword)
+            ->paginate($perPage);
+    }
+}
 
