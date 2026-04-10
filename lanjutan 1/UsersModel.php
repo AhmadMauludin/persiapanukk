68f9b9024115f21652f1d2f6036b29af8c1675dc
AhmadMauludin <ahmadmaldin1799@gmail.com>
2026-04-10 22:03:16 +0700

    Update UsersModel.php

--- lanjutan 1/UsersModel.php
+++ lanjutan 1/UsersModel.php
@@ -7,8 +7,8 @@ use CodeIgniter\Model;
 class UsersModel extends Model
 {
     protected $table = 'users';
-    protected $primaryKey = 'id_user';
-    protected $allowedFields = ['nama', 'username', 'password', 'role', 'foto'];
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['nama', 'email', 'username', 'password', 'role', 'foto','status'];
 
     public function getUsersByUsername($username)
     {
